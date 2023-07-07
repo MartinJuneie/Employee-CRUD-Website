a2d65f9a6205ec50ccc7e8a39d8b4bfab7973cd7
Martin Joseph Juneie <martinjuneie@gmail.com>
2023-07-08 00:52:49 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,99 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using CRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly CRUDDBContext cRUDDBContext;
+
+        public EmployeesController(CRUDDBContext cRUDDBContext)
+        {
+            this.cRUDDBContext = cRUDDBContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await cRUDDBContext.Employees.ToListAsync();
+            return View(employees);
+
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department
+            };
+            await cRUDDBContext.Employees.AddAsync(employee);
+            await cRUDDBContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await cRUDDBContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    DateOfBirth = employee.DateOfBirth,
+                    Department = employee.Department
+                };
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            return RedirectToAction("Index"); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await cRUDDBContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+                await cRUDDBContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await cRUDDBContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                cRUDDBContext.Employees.Remove(employee);
+                await cRUDDBContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Migrations/20230707120249_Second Migration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+    }
+}
